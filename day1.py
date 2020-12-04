6e33a0b21187ba36981b4a3c42532e75e59ff01a
PetterBerntsson <iprovedtheriemannhypothesis@gmail.com>
2020-12-04 11:11:52 +0100

    solutions for day 1

---
+++ day1.py
@@ -0,0 +1,36 @@
+with open ("data/day1.txt", "r") as f:
+    input = f.readlines()
+    entries = []
+
+    for line in input:
+        data = int(line.strip())
+        entries.append(data)
+
+    entries.sort()
+    smallest = entries[0]
+    biggest = entries[len(entries) - 1]
+    i = 0
+    while not smallest + biggest == 2020:
+        if smallest + biggest > 2020:
+            biggest = entries[len(entries) - i - 1]
+        else:
+            smallest = entries[i]
+        i += 1
+
+    print("Part 1: " + "\t" + str(smallest*biggest))
+
+    add_set = dict()
+    for i, e in enumerate(entries):
+        for j in range(len(entries) - i):
+            add_set[e + entries[j]] = (e, entries[j])
+
+    a1 = 0
+    a2 = 0
+    a3 = 0
+    for e in entries:
+        if (2020-e) in add_set:
+            a1 = e
+            a2, a3 = add_set[2020-e]
+            break
+
+    print(a1*a2*a3)
